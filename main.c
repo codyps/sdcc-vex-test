b760de36d144f6c7615a32809d6c95fcd562f94a
John Mesmon <jmesmon@gmail.com>
2010-05-28 19:43:26 -0400

    added pragma code usage. appears to function, but is wierd. reset ivt still gets placed at 0.

--- main.c
+++ main.c
@@ -8,12 +8,14 @@ void isr_low(void) {
 
 }
 
+#pragma code ivt_high 0x808
 void ivt_high(void) __naked {
 	__asm
 		goto _isr_high
 	__endasm;
 }
 
+#pragma code ivt_low 0x818
 void ivt_low(void) __naked {
 	__asm
 		goto _isr_low
