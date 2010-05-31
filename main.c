18fbac00217d8360dae3edf859072ade093a6729
jmesmon <jmesmon@gmail.com>
2010-05-31 02:56:40 -0400

    use the __interrupt keyword instead of the code pragma

--- main.c
+++ main.c
@@ -8,15 +8,15 @@ void isr_low(void) {
 
 }
 
-#pragma code ivt_high 0x808
-void ivt_high(void) __naked {
+//#pragma code ivt_high 0x808
+void ivt_high(void) __naked __interrupt 1 {
 	__asm
 		goto _isr_high
 	__endasm;
 }
 
-#pragma code ivt_low 0x818
-void ivt_low(void) __naked {
+//#pragma code ivt_low 0x818
+void ivt_low(void) __naked __interrupt 2 {
 	__asm
 		goto _isr_low
 	__endasm;
